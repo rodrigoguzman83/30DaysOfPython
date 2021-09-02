adec339dc1cf6e4bdac592852ca064c72347d73a
rodrigo <rodrigouisguzman@gmail.com>
2021-09-02 17:39:28 -0300

    exercise day01

---
+++ 01_Day_Introduction/helloworld.py
@@ -0,0 +1,20 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print('Suma', 2 + 3) #addition 
+print('Resta' , 4 - 1) #substraction
+print('Multiplicacion' , 3 * 2) #multiplication
+print('division', 9 / 3) #division 
+print('module', 4%2) #modulus
+print(3 // 2) #floor division operator
+
+#checking data types
+
+print(type(10)) #integer
+print(type(3.50)) #float
+print(type(10 + 3j)) #complex
+print(type("python")) #string
+print(type([10,5,3])) #list
+print(type({'name':'rodrigo'})) #dictionary
+print(type({9.8,2.3,3.14})) #set
+print(type((9.8,2.3,3.14))) #tuple
